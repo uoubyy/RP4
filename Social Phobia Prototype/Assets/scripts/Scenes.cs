7f4c356f51078a577a172110bebbd29d6e5d76de
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2021-10-25 16:25:01 -0600

    Two scenes, scene management, UI buttons, credits

---
+++ Social Phobia Prototype/Assets/scripts/Scenes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenes: MonoBehaviour
+{
+    public void loadGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+    }
+}
