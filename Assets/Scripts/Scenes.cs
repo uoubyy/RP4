084e65771329c9f04df186ea0cac9bf41aaf0395
Sage Severson <54301038+NakoaK@users.noreply.github.com>
2021-10-28 17:05:18 -0600

    Scene Manger, basic UI for health analysis

---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scenes : MonoBehaviour
+{
+    public void Analyze()
+    {
+        SceneManager.LoadScene("Analysis", LoadSceneMode.Single);
+    }
+    public void Pause()
+    {
+        SceneManager.LoadScene("Pause", LoadSceneMode.Single);
+    }
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+}
