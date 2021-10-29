c28cc035380dbd627ca586eed5312a7d19bad83e
u1317110 <yanyi.bao@utah.edu>
2021-10-28 20:40:21 -0600

    modify main scene

--- Assets/Scripts/AudioRecordController.cs
+++ Assets/Scripts/AudioRecordController.cs
@@ -8,6 +8,8 @@ public class AudioRecordController : MonoBehaviour
     public bool m_useAudio = false;
 
     public AudioClip m_testAudioTrack;
+    [Range(0.0f, 1.0f)]
+    public float m_testAudioVolume = 1.0f;
     public AudioSource m_audioSource;
     public float m_updateStep = 0.1f;
     public int m_sampleDataLength = 1024;
@@ -15,14 +17,16 @@ public class AudioRecordController : MonoBehaviour
     public float m_minScale = 0.5f;
     public float m_maxScale = 8.0f;
 
-    public float m_targetScale = 1.0f;
-    public float m_currentScale = 1.0f;
+    public float m_breathThreshold = 20.0f;
+
+    private float m_targetScale = 1.0f;
+    private float m_currentScale = 1.0f;
 
     public Transform[] m_visualObjects;
     public Text m_audioVolumeNum;
 
     public GameObject m_player;
-    public GameObject[] m_crowd;
+    public Rigidbody[] m_crowd;
     public float m_crowdMoveSpeed = 1.0f;
     private Vector3[] m_crowdTarget;
 
@@ -31,7 +35,10 @@ public class AudioRecordController : MonoBehaviour
     private float m_currentUpdateTime = 0.0f;
 
     private float[] m_clipSampleData;
-    private float m_clipLoudness = 0.0f;
+
+    private float m_rhythmSimilarity = 0.0f;
+
+    public BreathIndicator m_breathIndictor;
 
     // Start is called before the first frame update
     void Start()
@@ -55,6 +62,18 @@ public class AudioRecordController : MonoBehaviour
         m_crowdTarget = new Vector3[m_crowd.Length];
     }
 
+    void CalculateRhythmSimilarity(float loudness)
+    {
+        int state = m_breathIndictor.GetBreathState();
+        if ((state == 1 && loudness >= m_breathThreshold) ||
+        (state == -1 && loudness < m_breathThreshold))
+            m_rhythmSimilarity += 1;
+        else
+            m_rhythmSimilarity -= 1;
+
+        m_rhythmSimilarity = Mathf.Clamp(m_rhythmSimilarity, 0, 2.0f / m_updateStep);
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -66,45 +85,45 @@ public class AudioRecordController : MonoBehaviour
                 m_currentUpdateTime = 0.0f;
                 m_audioSource.clip.GetData(m_clipSampleData, m_audioSource.timeSamples);
 
-                m_clipLoudness = 0.0f;
+                float clipLoudness = 0.0f;
                 foreach (var sample in m_clipSampleData)
                 {
-                    m_clipLoudness += Mathf.Abs(sample);
+                    clipLoudness += Mathf.Abs(sample);
                 }
 
                 // m_clipLoudness /= m_sampleDataLength;
 
-                m_clipLoudness = m_clipLoudness * (m_useAudio ? 0.01f : 100.0f);
-                m_audioVolumeNum.text = string.Format("Audio input device {0},\nloudness {1}.", m_inputDevice, m_clipLoudness);
-                //Debug.Log("Voice loundness " + m_clipLoudness);
+                clipLoudness = clipLoudness * (m_useAudio ? m_testAudioVolume : 100.0f);
 
-                if (m_clipLoudness > 20.0f)
+                // after an interval, calculate similarity again
+                // similarity > 0.8 * 1.0f / m_updateStep move away
+                CalculateRhythmSimilarity(clipLoudness);
+
+                m_audioVolumeNum.text = string.Format("Audio input device {0}\nloudness {1}\nsimilarity{2}", m_inputDevice, clipLoudness, m_rhythmSimilarity);
+
+                if (clipLoudness > m_breathThreshold)
                     m_targetScale = m_maxScale;
                 else
                     m_targetScale = m_minScale;
 
-                int crowdsMoveDir = -1;
-                if (m_clipLoudness > 10.0f)
-                    crowdsMoveDir = 1;
-
                 for (var idx = 0; idx < m_crowd.Length; idx++)
                 {
-                    Vector3 dir = (m_crowd[idx].transform.position - m_player.transform.position) * crowdsMoveDir;
+                    Vector3 dir = (m_crowd[idx].transform.position - m_player.transform.position);//* (m_targetScale == m_minScale ? -1 : 1);
                     dir.Normalize();
-                    m_crowdTarget[idx] = m_crowd[idx].transform.position + dir * m_crowdMoveSpeed * m_updateStep;
+                    m_crowdTarget[idx] = dir;//m_crowd[idx].transform.position + dir * m_crowdMoveSpeed * m_updateStep;
                 }
             }
 
             float scaleSpeed = 0.0f;
             m_currentScale = Mathf.SmoothDamp(m_currentScale, m_targetScale, ref scaleSpeed, m_updateStep);
-            foreach (var obj in m_visualObjects)
+            foreach (var obj in m_visualObjects) // update the real-time voice indicator
             {
                 obj.localScale = new Vector3(1, m_currentScale, 1);
             }
 
             for (var idx = 0; idx < m_crowd.Length; idx++)
             {
-                m_crowd[idx].transform.position = Vector3.MoveTowards(m_crowd[idx].transform.position, m_crowdTarget[idx], m_updateStep);
+                m_crowd[idx].AddForce(m_crowdTarget[idx] * (m_rhythmSimilarity >= 0.8 * 2.0f / m_updateStep ? 1.0f : -1.0f) * 20);
             }
         }
     }
---
+++ Assets/Scripts/BreathIndicator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BreathIndicator : MonoBehaviour
+{
+    public float m_minScale = 1.0f;
+    public float m_maxScale = 3.0f;
+
+    public float m_timeElapsed = 0.0f;
+
+    public float m_timeDuration = 3.0f;
+
+    public float m_rotationSpeed = 1.0f;
+    private float m_rotationZ = 0.0f;
+    private int m_changeDir = 1;
+    private Image[] m_childImages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_childImages = GetComponentsInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rotationZ += Time.deltaTime * m_rotationSpeed;
+
+        m_timeElapsed += Time.deltaTime;
+        if (m_timeElapsed >= m_timeDuration)
+        {
+            m_timeElapsed = 0.0f;
+            m_changeDir = -m_changeDir;
+        }
+
+        foreach (Image img in m_childImages)
+        {
+            if (img.transform.parent == this.transform)
+            {
+                float start = m_changeDir > 0 ? m_minScale : m_maxScale;
+                float end = m_minScale + m_maxScale - start;
+                float scale = Mathf.Lerp(start, end, m_timeElapsed / m_timeDuration);
+                img.transform.localScale = new Vector3(scale, scale, 1);
+            }
+        }
+
+        this.transform.localRotation = Quaternion.Euler(0, 0, m_rotationZ);
+    }
+
+    // 1 for speaking
+    // -1 for quiet
+    public int GetBreathState()
+    {
+        return m_changeDir;
+    }
+}
